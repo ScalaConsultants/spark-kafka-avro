273d75018fb4d8fbc4b11a09e7ae808d579db931
Sebastian Bach <sbach@o2.pl>
2015-06-25 17:11:22 +0200

    This is a first working approach: Spark Stream just prints the whole topic "categories" to console.

--- build.sbt
+++ build.sbt
@@ -5,23 +5,33 @@ val metaSettings = Seq(
 )
 
 val scalaSettings = Seq(
-  scalaVersion := "2.11.6",
+  scalaVersion := "2.10.5",
   scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation", "-encoding", "utf8")
 )
 
-val sbtSettings = Seq(
-  connectInput in run := true,
-  fork in run := true,
-  javaOptions in run ++= Seq("-Xms512M", "-Xmx1G")
+val deploymentSettings = Seq(
+  mainClass in assembly := Some("io.scalac.spark.AvroConsumer"),
+  assemblyOutputPath in assembly := file("target/deploy/spark-kafka-avro.jar"),
+  test in assembly := {}
+)
+
+val repositories = Seq(
+  "confluent" at "http://packages.confluent.io/maven/",
+  Resolver.sonatypeRepo("public")
 )
 
 val dependencies = Seq(
-  "org.apache.spark" % "spark-streaming_2.10" % "1.4.0",
+  "org.apache.spark" % "spark-streaming_2.10" % "1.4.0"  % "provided",
   "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.4.0"
+    exclude("org.spark-project.spark", "unused"),
+  "org.apache.avro" % "avro" % "1.7.7",
+  "io.confluent" % "kafka-avro-serializer" % "1.0",
+  "com.github.scopt" %% "scopt" % "3.3.0"
 )
 
 lazy val root = (project in file(".")).
   settings(metaSettings: _*).
   settings(scalaSettings: _*).
-  settings(sbtSettings: _*).
+  settings(deploymentSettings: _*).
+  settings(resolvers ++= repositories).
   settings(libraryDependencies ++= dependencies)
--- src/main/scala/io/scalac/spark/AvroConsumer.scala
+++ src/main/scala/io/scalac/spark/AvroConsumer.scala
@@ -1,25 +1,51 @@
 package io.scalac.spark
 
-import kafka.serializer.StringDecoder
+import io.confluent.kafka.serializers.KafkaAvroDecoder
+import org.apache.avro.generic.GenericRecord
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{StreamingContext, Seconds}
 
+case class Config(postgres: Map[String, String] = Map(), broker: String = "", schemaRegistry: String = "")
+
 object AvroConsumer {
 
   def main(args: Array[String]) {
+    val parser = new scopt.OptionParser[Config]("spark") {
+      opt[Map[String, String]]("postgres") required() action { (x, c) =>
+        c.copy(postgres = x)
+      } keyValueName("<key>", "<value>") text ("are PostgreSQL connection params")
+
+      opt[String]("broker") required() action { (x, c) =>
+        c.copy(broker = x)
+      } valueName ("<broker1-host:port>,<broker2-host:port>") text ("is a list of one or more Kafka brokers")
+
+      opt[String]("schema-registry") required() action { (x, c) =>
+        c.copy(schemaRegistry = x)
+      } valueName ("<schema registry url>") text ("is the Avro schema-registry URL")
+    }
+
+    parser.parse(args, Config()) match {
+      case Some(config) => process(config)
+      case _ =>
+    }
+  }
+
+  def process(config: Config) = {
     val conf = new SparkConf().setAppName("DirectKafkaAvroConsumer")
     val ssc = new StreamingContext(conf, Seconds(10))
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> "tcp://localhost:1234")
+    val kafkaParams = Map[String, String](
+      "auto.offset.reset" -> "smallest",
+      "metadata.broker.list" -> config.broker,
+      "schema.registry.url" -> config.schemaRegistry)
     val topicSet = Set("categories")
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicSet)
+    val messages = KafkaUtils.createDirectStream[Object, Object, KafkaAvroDecoder, KafkaAvroDecoder](ssc, kafkaParams, topicSet)
 
     // Process lines
-    // val lines = SomeProcessor.process(messages.map(_._2))
-    // lines.print()
+     val lines = messages.map(_._2.toString)
+     lines.print()
 
     ssc.start()
     ssc.awaitTermination()
-//    ssc.stop()
   }
 }
